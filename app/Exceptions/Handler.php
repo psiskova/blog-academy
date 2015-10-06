585eafb90da14567748f270f72e5275cc5d385cd
D0d0 <jozef.d13@gmail.com>
2015-10-06 21:41:16 +0200

    Init project

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+class Handler extends ExceptionHandler {
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        HttpException::class,
+        ModelNotFoundException::class,
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception $e
+     * @return void
+     */
+    public function report(Exception $e) {
+        return parent::report($e);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $e
+     * @return Response
+     */
+    public function render($request, Exception $e) {
+
+        if (config('app.debug')) {
+            return $this->renderExceptionWithWhoops($e);
+        }
+
+        return parent::render($request, $e);
+    }
+
+    /**
+     * Render an exception using Whoops.
+     *
+     * @param  \Exception $e
+     * @return Response
+     */
+    protected function renderExceptionWithWhoops(Exception $e) {
+        $whoops = new Run;
+        $whoops->pushHandler(new PrettyPageHandler());
+
+        return new Response(
+            $whoops->handleException($e),
+            $e->getStatusCode(),
+            $e->getHeaders()
+        );
+    }
+}
