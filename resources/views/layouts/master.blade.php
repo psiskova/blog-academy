fdbc56d359fe1de24773a16c9debe49b9f6e7462
psiskova <pat.siskova@gmail.com>
2015-10-28 19:11:04 +0100

    Add HTML provider

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="_token" content="{{ csrf_token() }}">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    {!! HTML::style('css/bootstrap.min.css') !!}
+    {!! HTML::script('js/jquery.min.js') !!}
+    {!! HTML::script('js/bootstrap.min.js') !!}
     <script>
         $(document).ready(function () {
             $.ajaxSetup({
