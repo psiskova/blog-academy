16ecd977ad2b7427480ff40cf68d40d4536725dc
MartinaJ <jamriskova6@uniba.sk>
2015-11-05 20:28:47 +0100

    modify articles/show

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,16 +2,21 @@
 
 @section('left')
     <div class="row">
-        <p>tu budeme vypisovat cely clanok a diskusiu</p>
-
+        <h3>{!! $article->title !!}</h3>
+        {{ $article->updated_at->format('M j, Y') }}<br>
         <p>{!! $article->text !!}</p>
     </div>
+
+    <p>DISKUSIA K ČLÁNKU</p>
 @stop
 
 @section('right')
     <div class="row">
-        <p>tu budeme vypisovat profil autora</p>
-
-        <p>{{ $article->user->fullname }}</p>
+        {{--<img src="{{ url($user->image) }}">--}}
+        USER IMAGE
+        <h4>{{ $article->user->fullname }}</h4>
+        {{ $article->user->email }}<br>
+        {{--{!! link_to_action('**TODO presmerovanie', 'publikované články ('.**TODO pocet.')',['user_id' => $article->slug]) !!} --}}
+        **TODO publikované články(počet)
     </div>
 @stop
