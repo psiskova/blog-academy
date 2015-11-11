b310e2d85936c7d9ba3ab8c56adb989a9297e273
MartinaJ <jamriskova6@uniba.sk>
2015-11-11 17:25:56 +0100

    modify view user\block

--- resources/views/users/block.blade.php
+++ resources/views/users/block.blade.php
@@ -5,19 +5,27 @@
 @stop
 
 @section('left')
+    <h1 class="text-center">BLOKOVANIE POUŽÍVATEĽOV</h1>
     <div class="row">
-        <table>
+        <table class="center_elements table_block table-striped">
+            <tr>
+                <th class="border_right">Meno a priezvisko</th>
+                <th class="border_right">Zablokovať</th>
+                <th>Odblokovať</th>
+            </tr>
             @foreach($users as $user)
                 {!! Form::open(['url' => action('UserController@postBlock'), 'method' => 'post']) !!}
                 <tr>
-                    <td>{{ $user->fullname }}</td>
-                    <td>{!! Form::submitWithIcon('ban', 1, 'btn-danger' . ($user->ban == 1 ? ' disabled' : ''), 'glyphicon-remove') !!}</td>
-                    <td>{!! Form::submitWithIcon('ban', 0, 'btn-success'. ($user->ban == 0 ? ' disabled' : ''), 'glyphicon-ok') !!}</td>
+                    <td class="border_right">{{ $user->fullname }}</td>
+                    <td class="border_right">{!! Form::submitWithIcon('ban', 0, 'btn-success center-block'. ($user->ban == 0 ? ' disabled' : ''), 'glyphicon-ok') !!}</td>
+                    <td>{!! Form::submitWithIcon('ban', 1, 'btn-danger center-block' . ($user->ban == 1 ? ' disabled' : ''), 'glyphicon-remove') !!}</td>
                     {!! Form::hidden('id', $user->id) !!}
                 </tr>
                 {!! Form::close() !!}
             @endforeach
         </table>
-        {!! $users->render() !!}
+        <div class="text-center">
+            {!! $users->render() !!}
+        </div>
     </div>
 @stop
