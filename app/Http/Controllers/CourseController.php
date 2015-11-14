9562a03c5abc9f8293d0fda33000c54398253da8
D0d0 <jozef.d13@gmail.com>
2015-11-14 21:57:27 +0100

    Courses

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -27,7 +27,6 @@ class CourseController extends Controller {
     public function getCreate() {
 
         return view('courses.create');
-
     }
 
     public function postCreate(Request $request) {
