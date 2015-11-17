69b34ec9233442ed5fd207530cf86bb60ed7fe22
D0d0 <jozef.d13@gmail.com>
2015-11-17 20:37:34 +0100

    Add macro

---
+++ resources/macros/tags.macro.php
@@ -0,0 +1,9 @@
+<?php
+
+HTML::macro('tags', function ($article) {
+    $collection = collect(array_flatten($article->tags()->get(['name'])->toArray()))->map(function ($tag) {
+        return '<span class="tag label label-info"><a href="">' . $tag . '</a></span>';
+    })->all();
+
+    return implode(' ', $collection);
+});
