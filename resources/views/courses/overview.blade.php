7b9893c5975f9da9bf3375c38ac85b74d3ffc65b
MartinaJ <jamriskova6@uniba.sk>
2015-11-18 18:07:28 +0100

    modify tables (col-width)

--- resources/views/courses/overview.blade.php
+++ resources/views/courses/overview.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.9-3')
 
 @section('left')
-    <h1 class="text-center">PRIHLÁSENIE NA PREDMET</h1>
-    @foreach($courses as $course)
-        <table class="center_elements table_courses_view table-striped">
-            <tr>
-                <td class="border_right"><b>{{ $course->name }}</b>, {{ $course->teacher->fullname }}</td>
-                {{--podľa stavu prihláseného užívateľa v danom predmete sa mu ponúkne adekvátny výpis--}}
-                <td>
-                    <button type="button" class="btn btn-default">Prihlásiť sa</button>
-                </td>
-                <td>Čaká na schválenie</td>
-                <td>Prihlásený</td>
-            </tr>
-        </table>
-    @endforeach
+    <div class="row">
+        <h1>Prihlásenie na predmet</h1>
+        @foreach($courses as $course)
+            <table class="center_elements table_courses_view table-striped col-xs-12">
+                <tr class="row">
+                    <td class="border_right col-xs-6"><b>{{ $course->name }}</b>, {{ $course->teacher->fullname }}</td>
+                    {{--podľa stavu prihláseného užívateľa v danom predmete sa mu ponúkne adekvátny výpis--}}
+                    <td class ="col-xs-6">
+                        <button type="button" class="btn btn-default center-block">Prihlásiť sa</button>
+                    </td>
+    {{--                <td class ="col-xs-6">Čaká na schválenie</td>
+                    <td class ="col-xs-6">Prihlásený</td>--}}
+                </tr>
+            </table>
+        @endforeach
+    </div>
 @stop
 
 @section('right')
--- resources/views/users/grading.blade.php
+++ resources/views/users/grading.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts/9-3')
 
 @section('left')
+    <div class="row">
     <h1>Hodnotenie</h1>
-    <div class="side_tabs">
+    <div class="side_tabs col-sx-12 side_tabs_grading">
         <ul class="nav nav-tabs">
             <li class="active"><a data-toggle="tab" href="#tab1">Neohodnotené</a></li>
             <li><a data-toggle="tab" href="#tab2">Celkové hodnotenie</a></li>
@@ -11,40 +12,42 @@
             <div id="tab1" class="tab-pane fade in active">
 
                 @if(Auth::user()->hasRole(\App\Models\User::TEACHER_ROLE))
-                    <table class="center_elements table_not_grading_teacher table-striped">
-                        <tr>
-                            <td class="border_right">Názov</td>
-                            <td>
-                                <button type="button" class="btn btn-default">Ohodnotiť</button>
+                    <table class="center_elements table-striped table_not_grading_teacher col-xs-12">
+                        <tr class="row">
+                            <td class="border_right col-xs-6">Názov</td>
+                            <td class="col-xs-6">
+                                <button type="button" class="btn btn-default center-block">Ohodnotiť</button>
                             </td>
                         </tr>
                     </table>
                 @else
-                    <table class="center_elements table-striped table_not_grading_student">
-                        <tr>
-                            <td>Článok 1</td>
+                    <table class="center_elements table-striped table_not_grading_student col-xs-12">
+                        <tr class="row">
+                            <td class="col-xs-12">Článok 1</td>
                         </tr>
                     </table>
                 @endif
             </div>
             <div id="tab2" class="tab-pane fade">
                 @if(Auth::user()->hasRole(\App\Models\User::TEACHER_ROLE))
-                    <table class="center_elements table-striped table_grades">
-                        <tr>
-                            <td class="border_right">Meno študenta</td>
-                            <td>Celkové hodnotenie študenta v danom predmete</td>
+                    <table class="center_elements table-striped table_grades col-xs-12">
+                        <tr class="row">
+                            <td class="border_right col-xs-6">Meno študenta</td>
+                            <td class="col-xs-6">Celkové hodnotenie študenta v danom predmete</td>
                         </tr>
                     </table>
                 @else
-                    <table class="center_elements table-striped table_grades">
-                        <tr>
-                            <td class="border_right">Článok 1</td>
-                            <td>Hodnotenie</td>
+                    <table class="center_elements table-striped table_grades col-xs-12">
+                        <tr class="row">
+                            <td class="border_right col-xs-6">Článok 1</td>
+                            <td class="col-xs-6">Hodnotenie</td>
                         </tr>
                     </table>
                 @endif
             </div>
         </div>
+    </div>
+
     </div>
 
 @stop
