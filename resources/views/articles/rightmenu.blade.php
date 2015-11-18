4966de570d6cc9db85f7a6c468f7a10050cda5c8
D0d0 <jozef.d13@gmail.com>
2015-11-18 22:37:26 +0100

    Add count into right menu

--- resources/views/articles/rightmenu.blade.php
+++ resources/views/articles/rightmenu.blade.php
@@ -1,7 +1,10 @@
 <div class="row">
     <p>tu budeme vypisovat profil autora</p>
     {!! link_to_action('ArticleController@getCreate', "Nový článok") !!} <br>
-    {!! link_to_action('ArticleController@getMyArticles', "Publikované články (počet)") !!} <br>
-    {!! link_to_action('ArticleController@getMyDrafts', "Koncepty (počet)") !!} <br>
+    {{--*/ $count = $count = \App\Models\Article::where('user_id', '=', Auth::id())->published()->count() /*--}}
+    {!! link_to_action('ArticleController@getMyArticles', "Publikované články " . ($count ? ('('.$count.')') : '')) !!}
+    <br>
+    {{--*/ $count = $count = \App\Models\Article::where('user_id', '=', Auth::id())->draft()->count() /*--}}
+    {!! link_to_action('ArticleController@getMyDrafts', "Koncepty " . ($count ? ('('.$count.')') : '')) !!}
+    <br>
 </div>
-
