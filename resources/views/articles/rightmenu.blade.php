7d591da477f6a612e448b69a6dbc269959a7fd20
D0d0 <jozef.d13@gmail.com>
2015-11-18 22:38:24 +0100

    Modify count in right menu

--- resources/views/articles/rightmenu.blade.php
+++ resources/views/articles/rightmenu.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
     <p>tu budeme vypisovat profil autora</p>
     {!! link_to_action('ArticleController@getCreate', "Nový článok") !!} <br>
-    {{--*/ $count = $count = \App\Models\Article::where('user_id', '=', Auth::id())->published()->count() /*--}}
+    {{--*/ $count = \App\Models\Article::where('user_id', '=', Auth::id())->published()->count() /*--}}
     {!! link_to_action('ArticleController@getMyArticles', "Publikované články " . ($count ? ('('.$count.')') : '')) !!}
     <br>
-    {{--*/ $count = $count = \App\Models\Article::where('user_id', '=', Auth::id())->draft()->count() /*--}}
+    {{--*/ $count = \App\Models\Article::where('user_id', '=', Auth::id())->draft()->count() /*--}}
     {!! link_to_action('ArticleController@getMyDrafts', "Koncepty " . ($count ? ('('.$count.')') : '')) !!}
     <br>
 </div>
