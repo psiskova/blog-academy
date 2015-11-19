2d527cfe4d106e7149ce24152fa347453a940039
psiskova <pat.siskova@gmail.com>
2015-11-19 14:05:33 +0100

    rating - model

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
-    //
+    protected $table = 'ratings';
+
+    protected $fillable = [
+        'user_id',
+        'article_id',
+        'text'
+    ];
 }
