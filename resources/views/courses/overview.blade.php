0bd4e92c01fca06612e01f30e33b0cca9e05194e
MartinaJ <jamriskova6@uniba.sk>
2015-11-19 04:52:24 +0100

    add attr. table-striped

--- resources/views/courses/overview.blade.php
+++ resources/views/courses/overview.blade.php
@@ -3,19 +3,19 @@
 @section('left')
     <div class="row">
         <h1>Prihlásenie na predmet</h1>
-        @foreach($courses as $course)
-            <table class="center_elements table_courses_view table-striped col-xs-12">
-                <tr class="row">
+        <table class="center_elements table_courses_view table-striped col-xs-12">
+            @foreach($courses as $course)
+                <tr  class="row">
                     <td class="border_right col-xs-6"><b>{{ $course->name }}</b>, {{ $course->teacher->fullname }}</td>
                     {{--podľa stavu prihláseného užívateľa v danom predmete sa mu ponúkne adekvátny výpis--}}
                     <td class ="col-xs-6">
                         <button type="button" class="btn btn-default center-block">Prihlásiť sa</button>
                     </td>
-    {{--                <td class ="col-xs-6">Čaká na schválenie</td>
-                    <td class ="col-xs-6">Prihlásený</td>--}}
+        {{--                <td class ="col-xs-6">Čaká na schválenie</td>
+                        <td class ="col-xs-6">Prihlásený</td>--}}
                 </tr>
-            </table>
-        @endforeach
+            @endforeach
+        </table>
     </div>
 @stop
 
