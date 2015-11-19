90ec4b4c2f8d571a8df619ffb5cdd7bf3e8ac53c
psiskova <pat.siskova@gmail.com>
2015-11-19 14:40:40 +0100

    rate article: plugin for stars

--- resources/views/articles/ratearticle.blade.php
+++ resources/views/articles/ratearticle.blade.php
@@ -4,12 +4,14 @@
     {!! HTML::script('js/summernote.min.js') !!}
     {!! HTML::script('js/bootstrap-tagsinput.min.js') !!}
     {!! HTML::script('js/article.create.js') !!}
+    {!! HTML::script('js/star-rating.min.js') !!}
 @stop
 
 
 @section('left')
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
     {!! HTML::style('css/summernote.css') !!}
+    {!! HTML::style('css/star-rating.min.css') !!}
 
 
     <div class="row">
@@ -25,26 +27,12 @@
         {!! Form::hidden('id', isset($article) ? $article->slug : '') !!}
         <div class="form-group">
             <label for="grades">Hodnotenie:</label>
-            <select class="form-control course-option" id="grades">
-                <option value="1">1/5 <i class="icon ion-star"></i> </option>
-                <option value="2">
-                    2/5 <i class="icon ion-star"></i><i class="icon ion-star"></i>
-                </option>
-                <option value="3">
-                    3/5 <i class="icon ion-star"></i><i class="icon ion-star"></i><i class="icon ion-star"></i>
-                </option>
-                <option value="4">
-                    4/5 <i class="icon ion-star"></i><i class="icon ion-star"></i><i class="icon ion-star"></i><i class="icon ion-star"></i>
-                </option>
-                <option value="5">
-                    5/5 <i class="icon ion-star"></i><i class="icon ion-star"></i><i class="icon ion-star"></i><i class="icon ion-star"></i><i class="icon ion-star"></i>
-                </option>
-            </select>
+            <input id="input-id" type="number" class="rating" min=0 max=5 step=1 data-size="sm" data-show-Caption="false" data-show-Clear="false">
         </div>
         <div class="form-group">
-            <label for="summernote" class="col-md-2 control-label">Text hodnotenia</label>
-
-            <div class="col-md-10">
+            <label for="summernote" class="control-label">Text hodnotenia</label>
+            <div>
+                <br>
                 <textarea id="area" name="text"></textarea>
 
                 <div id="summernote"></div>
