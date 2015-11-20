d7f879738204db98b39393bbf1adbeb1cfa9019c
D0d0 <jozef.d13@gmail.com>
2015-11-20 23:55:31 +0100

    Update profile picture macro

--- resources/macros/profilePicture.macro.php
+++ resources/macros/profilePicture.macro.php
@@ -6,5 +6,5 @@ HTML::macro('profilePicture', function ($user, $width, $height, array $attribute
         $params .= $key . '="' . $value . '" ';
     }
 
-    return '<img ' . $params . ' onerror="this.src=\'img/no_profile_pic.png\'" src="' . action('UserController@getProfileImage', ['id' => $user->profileimage]) . '"style="height: ' . $height . 'px; width: ' . $width . 'px">';
+    return '<img ' . $params . ' onerror="this.src=\'' . asset('img/no_profile_pic.png') . '\'" src="' . action('UserController@getProfileImage', ['id' => $user->profileimage]) . '"style="height: ' . $height . 'px; width: ' . $width . 'px">';
 });
