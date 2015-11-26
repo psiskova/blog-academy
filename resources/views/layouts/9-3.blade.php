50facf874d7bbe7a4f488178128f5e8b047e72f5
MartinaJ <jamriskova6@uniba.sk>
2015-11-26 17:55:28 +0100

    edit footer

--- resources/views/layouts/9-3.blade.php
+++ resources/views/layouts/9-3.blade.php
@@ -3,11 +3,10 @@
 @section('content')
     <div class="container-fluid row text-justify container_content">
         <div class="col-md-7 right-column left_col">
-                @yield('left')
-            </div>
-            <div class="col-md-3 col-md-offset-1 right_col">
-                @yield('right')
-            </div>
+            @yield('left')
+        </div>
+        <div class="col-md-3 col-md-offset-1 right_col">
+            @yield('right')
         </div>
     </div>
 @stop
