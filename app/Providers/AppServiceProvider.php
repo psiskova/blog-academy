437dec3b5f4c0465dae9b0db72a0752bcceb3984
D0d0 <jozef.d13@gmail.com>
2015-11-26 15:43:19 +0100

    Turn off debug bar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider {
         if ($this->app->environment('production')) {
         } else {
             $this->app->register('Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider');
-            $this->app->register('Barryvdh\Debugbar\ServiceProvider');
+            //$this->app->register('Barryvdh\Debugbar\ServiceProvider');
         }
     }
 }
