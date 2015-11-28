e8284040b45ac230a5c41a2fd05bfee3b3f2e884
stiffix <stiffsoftware@gmail.com>
2015-11-28 17:13:56 +0100

    Dropdown position from bottom fixed, logo margin added

--- public/js/mobile-more-dropdown.js
+++ public/js/mobile-more-dropdown.js
@@ -3,9 +3,11 @@
 $(document).ready(function () {
     var rt = ($(window).width() - ($("a.tab-item.color-nav-course").offset().left + $('a.tab-item.color-nav-course').outerWidth()));
     var rw = $('a.tab-item.color-nav-course').outerWidth();
+    var rheight = $('a.tab-item.color-nav-course').height();
+
+    $('.more-dropdown-menu').css('bottom', rheight);
     $('.more-dropdown-menu').css('right', rt);
     $('.more-dropdown-menu').css('width', rw);
-    $('.more-dropdown-menu').css('display', 'none');
 
     $('a.tab-item.color-nav-course').click(function() {
         $('.more-dropdown-menu').slideToggle('fast');
@@ -13,7 +15,9 @@ $(document).ready(function () {
 
     $( window ).resize(function() {
         rt = ($(window).width() - ($("a.tab-item.color-nav-course").offset().left + $('a.tab-item.color-nav-course').outerWidth()));
-        var rw = $('a.tab-item.color-nav-course').outerWidth();
+        rw = $('a.tab-item.color-nav-course').outerWidth();
+        rheight = $('a.tab-item.color-nav-course').height();
+        $('.more-dropdown-menu').css('bottom', rheight);
         $('.more-dropdown-menu').css('right', rt);
         $('.more-dropdown-menu').css('width', rw);
     });
