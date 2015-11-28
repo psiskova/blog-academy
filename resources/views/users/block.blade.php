8122b6245ada15493e0110c49236c3ad3cb1cbd7
MartinaJ <jamriskova6@uniba.sk>
2015-11-28 22:42:34 +0100

    upravené tabuľky admina

--- resources/views/users/block.blade.php
+++ resources/views/users/block.blade.php
@@ -6,19 +6,19 @@
 
 @section('left')
     <div class="row">
-        <h1 class="text-center">BLOKOVANIE POUŽÍVATEĽOV</h1>
+        <h1>Blokovanie používateľov</h1>
         <table class="center_elements table_block table-striped col-xs-12">
             <tr class="row">
                 <th class="border_right col-xs-4">Meno a priezvisko</th>
-                <th class="border_right col-xs-4">Zablokovať</th>
-                <th class="col-xs-4">Odblokovať</th>
+                <th class="border_right col-xs-4 text-center">Zablokovať</th>
+                <th class="col-xs-4 text-center">Odblokovať</th>
             </tr>
             @foreach($users as $user)
                 {!! Form::open(['url' => action('UserController@postBlock'), 'method' => 'post']) !!}
                 <tr class="row">
-                    <td class="border_right col-xs-4">{{ $user->fullname }}</td>
-                    <td class="border_right col-xs-4">{!! Form::submitWithIcon('ban', 0, 'btn-success center-block'. ($user->ban == 0 ? ' disabled' : ''), 'glyphicon-ok') !!}</td>
-                    <td class="col-xs-4">{!! Form::submitWithIcon('ban', 1, 'btn-danger center-block' . ($user->ban == 1 ? ' disabled' : ''), 'glyphicon-remove') !!}</td>
+                    <td class="border_right col-xs-4 table_block_td">{{ $user->fullname }}</td>
+                    <td class="border_right col-xs-4 table_block_button">{!! Form::submitWithIcon('ban', 0, 'btn-success center-block'. ($user->ban == 0 ? ' disabled' : ''), 'glyphicon-ok') !!}</td>
+                    <td class="col-xs-4 table_block_button">{!! Form::submitWithIcon('ban', 1, 'btn-danger center-block' . ($user->ban == 1 ? ' disabled' : ''), 'glyphicon-remove') !!}</td>
                     {!! Form::hidden('id', $user->id) !!}
                 </tr>
                 {!! Form::close() !!}
@@ -26,6 +26,27 @@
         </table>
         <div class="text-center fix1">
             {!! $users->render() !!}
+        </div>
+    </div>
+@stop
+
+@section('right')
+    <div class="right_col_profile right_col_profile_bottom mobile_profile">
+        <div class="pencil"></div>
+        <div class="text-center">
+            {!! HTML::profilePicture(Auth::user(), 120, 120) !!}
+            <h4>{{ Auth::user()->fullname }}</h4>
+            <p>{{ Auth::user()->email }}</p>
+            @if(Auth::check())
+                <p>{!! link_to_action('UserController@getUpdateProfile', 'Upraviť profil', [])!!}</p>
+                <div class = "separator"></div>
+                <p>{!! link_to_action('ArticleController@getCreate', "Nový článok") !!}</p>
+                {{--*/ $count = \App\Models\Article::where('user_id', '=', Auth::id())->published()->count() /*--}}
+                <p>{!! link_to_action('ArticleController@getMyArticles', "Publikované články " . ($count ? ('('.$count.')') : '')) !!}</p>
+                {{--*/ $count = \App\Models\Article::where('user_id', '=', Auth::id())->draft()->count() /*--}}
+                <p>{!! link_to_action('ArticleController@getMyDrafts', "Koncepty " . ($count ? ('('.$count.')') : '')) !!}</p>
+                <p>{!! link_to_action('UserController@getManagement', "Správa používateľov") !!}</p>
+            @endif
         </div>
     </div>
 @stop
--- resources/views/users/management.blade.php
+++ resources/views/users/management.blade.php
@@ -6,16 +6,16 @@
 
 @section('left')
     <div class="row">
-        <h1 class="text-center">SPRÁVA POUŽÍVATEĽSKÝCH ROLÍ</h1>
-        <table class="center_elements table_block table-striped">
+        <h1>Správa používateľských rolí</h1>
+        <table class="center_elements table_manage table-striped col-xs-12">
             <tr>
-                <th class="border_right">Meno a priezvisko</th>
-                <th>Používateľská rola</th>
+                <th class="border_right col-xs-6">Meno a priezvisko</th>
+                <th class="col-xs-6">Používateľská rola</th>
             </tr>
             @foreach($users as $user)
                 <tr>
-                    <td class="border_right">{{ $user->fullname }}</td>
-                    <td>
+                    <td class="border_right table_manage_td col-xs-6">{{ $user->fullname }}</td>
+                    <td class="col-xs-6 table_manage_button">
                         {!! Form::select('role', [\App\Models\User::STUDENT_ROLE => 'Žiak', \App\Models\User::TEACHER_ROLE => 'Učiteľ', \App\Models\User::ADMIN_ROLE => 'Administrátor'], $user->role, ['id' => $user->id]) !!}
                     </td>
                 </tr>
@@ -23,6 +23,27 @@
         </table>
         <div class="text-center fix1">
             {!! $users->render() !!}
+        </div>
+    </div>
+@stop
+
+@section('right')
+    <div class="right_col_profile right_col_profile_bottom mobile_profile">
+        <div class="pencil"></div>
+        <div class="text-center">
+            {!! HTML::profilePicture(Auth::user(), 120, 120) !!}
+            <h4>{{ Auth::user()->fullname }}</h4>
+            <p>{{ Auth::user()->email }}</p>
+            @if(Auth::check())
+                <p>{!! link_to_action('UserController@getUpdateProfile', 'Upraviť profil', [])!!}</p>
+                <div class = "separator"></div>
+                <p>{!! link_to_action('ArticleController@getCreate', "Nový článok") !!}</p>
+                {{--*/ $count = \App\Models\Article::where('user_id', '=', Auth::id())->published()->count() /*--}}
+                <p>{!! link_to_action('ArticleController@getMyArticles', "Publikované články " . ($count ? ('('.$count.')') : '')) !!}</p>
+                {{--*/ $count = \App\Models\Article::where('user_id', '=', Auth::id())->draft()->count() /*--}}
+                <p>{!! link_to_action('ArticleController@getMyDrafts', "Koncepty " . ($count ? ('('.$count.')') : '')) !!}</p>
+                <p>Správa používateľov</p>
+            @endif
         </div>
     </div>
 @stop
