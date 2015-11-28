3f4a65130a5608182e54fb120eb2d37d9c20c939
MartinaJ <jamriskova6@uniba.sk>
2015-11-28 21:28:18 +0100

    chýbajúca medzera pri publikované články

--- resources/views/courses/teacher/overview.blade.php
+++ resources/views/courses/teacher/overview.blade.php
@@ -25,7 +25,7 @@
                             <div class = "separator"></div>
                             <p>{!! link_to_action('ArticleController@getCreate', "Nový článok") !!}</p>
                             {{--*/ $count = \App\Models\Article::where('user_id', '=', Auth::id())->published()->count() /*--}}
-                            <p>{!! link_to_action('ArticleController@getMyArticles', "Publikované články" . ($count ? ('('.$count.')') : '')) !!}</p>
+                            <p>{!! link_to_action('ArticleController@getMyArticles', "Publikované články " . ($count ? ('('.$count.')') : '')) !!}</p>
                             {{--*/ $count = \App\Models\Article::where('user_id', '=', Auth::id())->draft()->count() /*--}}
                             <p>{!! link_to_action('ArticleController@getMyDrafts', "Koncepty" . ($count ? ('('.$count.')') : '')) !!}</p>
                         @endif
