f91ed23cce127be57aec5458486f52ec312b7db7
MartinaJ <jamriskova6@uniba.sk>
2015-11-28 12:29:14 +0100

    fixnuté okraje na mobilnej verzii

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container-fluid row container_content">
-        <div class="col-md-offset-2 col-md-8 push">
+    <div class="container-fluid row">
+        <div class="col-md-8 col-md-offset-2 push container_content">
             <h1>O nás</h1>
             <img src="https://web-school.in/wp-content/uploads/2015/03/SMS.gif" class="center-block" style="width: 100%">
         </div>
