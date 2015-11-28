bf5c59ee73a5daf8cf4feea345337672b2cb9185
MartinaJ <jamriskova6@uniba.sk>
2015-11-28 21:45:22 +0100

    fixnuté prekrývanie obsahu s pätičkou v auth

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="container-fluid container_content">
+    <div class="container-fluid container_content push">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel">
