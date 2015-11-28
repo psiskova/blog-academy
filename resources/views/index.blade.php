2c2cfe4db2cf8c817fbc04745743f60a713a9262
psiskova <pat.siskova@gmail.com>
2015-11-28 11:00:30 +0100

    fix stars in index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,19 @@
 @extends('layouts.9-3')
 
+@section('scripts')
+    {!! HTML::script('js/star-rating.min.js') !!}
+@stop
+
 @section('left')
+    {!! HTML::style('css/star-rating.min.css') !!}
     <div class="row">
         @foreach($articles as $article)
             <div class="articles_list">
                 <h3>{!! link_to_action('ArticleController@getShow', $article->title, ['id' => $article->slug]) !!}</h3>
                 <span class="article-info">{!! link_to_action('UserController@getProfile', $article->user->fullname , ['user_id' => $article->user->slug]) !!}
-                    | {{$article->updated_at}} | {{ $article->average_rating }}</span><br>
+                    | {{$article->updated_at}} | <input id="input-id-avg" type="number" class="rating" min=0 max=5 step=1 readonly="true"  data-size="xs"
+                                                        data-show-Caption="false" data-show-Clear="false" value="{{ round($article->average_rating) }}">
+                </span><br>
                 {!! HTML::tags($article) !!}
                 {{ str_limit(strip_tags($article->text), 200) }}
             </div>
