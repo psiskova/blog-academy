93dfa519d319ba236ec30b613d349d5c3254f444
MartinaJ <jamriskova6@uniba.sk>
2015-11-29 14:08:59 +0100

    fixnutý náhľad článku view/show - mobil

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -12,16 +12,16 @@
         <span class="article-info">
             <p>{!! link_to_action('UserController@getProfile', $article->user->fullname, ['user_id' => $article->user->slug])!!}</p>
             <div class="divider"></div>{{ $article->updated_at }}
-            <div class="divider hidden-sm hidden-xs"></div>
-            <div class="hidden-sm hidden-xs">
-                <input id="input-id-avg" type="number" class="rating" min=0 max=5 step=1 readonly="true" data-size="xs"
-                       data-show-Caption="false" data-show-Clear="false" value="{{ round($article->average_rating) }}">
-                @if(Auth::check() && (Auth::user()->hasRole(\App\Models\User::ADMIN_ROLE) || Auth::user()->hasRole(\App\Models\User::TEACHER_ROLE)))
-                    <div class="divider"></div><a
-                            href="{{ action('ArticleController@getDelete', ['id' => $article->id]) }}" style="color:red">Zmazať
-                        nevhodný článok</a>
-                @endif
-            </div>
+            <div class="divider hidden-xs hidden-sm"></div>
+            <div class="hidden-xs hidden-sm">
+            <input id="input-id-avg" type="number" class="rating" min=0 max=5 step=1 readonly="true" data-size="xs"
+                   data-show-Caption="false" data-show-Clear="false" value="{{ round($article->average_rating) }}"></div>
+            @if(Auth::check() && (Auth::user()->hasRole(\App\Models\User::ADMIN_ROLE) || Auth::user()->hasRole(\App\Models\User::TEACHER_ROLE)))
+
+                <div class="divider hidden-xs hidden-sm"></div><div class="hidden-xs hidden-sm"><a
+                        href="{{ action('ArticleController@getDelete', ['id' => $article->id]) }}" style="color:red">Zmazať
+                    nevhodný článok</a></div>
+            @endif
         </span>
         <div class="hidden-md hidden-lg">
             <input id="input-id-avg" type="number" class="rating" min=0 max=5 step=1 readonly="true"
