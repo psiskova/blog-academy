edce8b9e74fa344bb2de7463543424b1c60882f2
stiffix <stiffsoftware@gmail.com>
2015-12-06 12:54:48 +0100

    movableprofile add prevention

--- public/js/movableprofile.js
+++ public/js/movableprofile.js
@@ -3,6 +3,11 @@ $( document ).ready(function() {
     var isMobile = window.matchMedia("only screen and (max-width: 991px)");
     var boundingElement = $('.right_col');
     var profileElement = $('#movable-bounding');
+    console.log(profileElement);
+
+    var isAble = ((boundingElement.length > 0) && (profileElement.length > 0));
+    if (!isAble) return;
+
     var DEFAULT_HEIGHT_PENCIL = boundingElement.height();
     var DEFAULT_WIDTH_PROFILE = boundingElement.width();
     //default : static, auto
@@ -12,22 +17,26 @@ $( document ).ready(function() {
     var boundingleft = boundingElement.offset().left;
 
     function resetPosition() {
-        profileElement.css('position', 'static');
-        profileElement.css('top', 'auto');
-        profileElement.css('left', 'auto');
-        profileElement.css('width', DEFAULT_WIDTH_PROFILE);
-        boundingElement.css('height', 'auto');
+        if (isAble) {
+            profileElement.css('position', 'static');
+            profileElement.css('top', 'auto');
+            profileElement.css('left', 'auto');
+            profileElement.css('width', DEFAULT_WIDTH_PROFILE);
+            boundingElement.css('height', 'auto');
+        }
     }
 
     function estimateMove() {
-        if(boundingtop <= 0) {
-            profileElement.css('position', 'fixed');
-            profileElement.css('top', '0px');
-            profileElement.css('left', (boundingleft + 15) + 'px');
-            profileElement.css('width', DEFAULT_WIDTH_PROFILE);
-            boundingElement.css('height', DEFAULT_HEIGHT_PENCIL + 'px');
-        } else {
-            resetPosition();
+        if (isAble) {
+            if (boundingtop <= 0) {
+                profileElement.css('position', 'fixed');
+                profileElement.css('top', '0px');
+                profileElement.css('left', (boundingleft + 15) + 'px');
+                profileElement.css('width', DEFAULT_WIDTH_PROFILE);
+                boundingElement.css('height', DEFAULT_HEIGHT_PENCIL + 'px');
+            } else {
+                resetPosition();
+            }
         }
     }
 
