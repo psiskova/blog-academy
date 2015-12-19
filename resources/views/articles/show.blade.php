21594c5c02566806e2f8e80203da12ae416a1f42
D0d0 <jozef.d13@gmail.com>
2015-12-19 14:30:33 +0100

    Remove duplicate ids

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -16,7 +16,7 @@
                 <div class="divider"></div>{{ $article->updated_at }}
                 <div class="divider hidden-xs hidden-sm"></div>
                 <div class="hidden-xs hidden-sm">
-                    <input id="input-id-avg" type="number" class="rating" min=0 max=5 step=1 readonly="true" data-size="xs"
+                    <input type="number" class="rating" min=0 max=5 step=1 readonly="true" data-size="xs"
                            data-show-Caption="false" data-show-Clear="false" value="{{ round($article->average_rating) }}">
                 </div>
                 @if(Auth::check() && (Auth::user()->hasRole(\App\Models\User::ADMIN_ROLE) || Auth::user()->hasRole(\App\Models\User::TEACHER_ROLE)))
@@ -30,7 +30,7 @@
                 @endif
             </span>
             <div class="hidden-md hidden-lg">
-                <input id="input-id-avg" type="number" class="rating" min=0 max=5 step=1 readonly="true"
+                <input type="number" class="rating" min=0 max=5 step=1 readonly="true"
                        data-size="xs"
                        data-show-Caption="false" data-show-Clear="false"
                        value="{{ round($article->average_rating) }}">
