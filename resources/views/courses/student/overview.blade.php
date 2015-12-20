b22a2ee6836113c8672ab62ab7ae81b48b5b5071
MartinaJ <jamriskova6@uniba.sk>
2015-12-20 03:21:34 +0100

    pridané tagy <thead> a <tbody> do tabuliek

--- resources/views/courses/student/overview.blade.php
+++ resources/views/courses/student/overview.blade.php
@@ -8,30 +8,32 @@
     <div class="row">
         <h1>Prihlásenie na predmet</h1>
         <table class="center_elements table_courses_view table-striped col-xs-12">
-            @foreach($courses as $course)
-                <tr class="row">
-                    <td class="border_right col-xs-6"><b>{{ $course->name }}</b>, {{ $course->teacher->fullname }}</td>
-                        @if($course->participants->first() && $course->participants->first()->state <> \App\Models\Participant::NOTHING)
-                            <td class="col-xs-6 text-center table_courses_view_td">
-                                @if($course->participants->first()->state == \App\Models\Participant::ACCEPTED)
-                                    Prihlásený
-                                @endif
-                                @if($course->participants->first()->state == \App\Models\Participant::REJECTED)
-                                    Odmietnutý
-                                @endif
-                                @if($course->participants->first()->state == \App\Models\Participant::PENDING)
-                                    Čaká na schválenie
-                                @endif
-                            </td>
-                        @else
-                            <td class="col-xs-6 text-center table_courses_view_button">
-                                <button type="button" class="btn btn-default" data-course="{{ $course->id }}">
-                                    Prihlásiť sa
-                                </button>
-                            </td>
-                        @endif
-                </tr>
-            @endforeach
+            <tbody>
+                @foreach($courses as $course)
+                    <tr class="row">
+                        <td class="border_right col-xs-6"><b>{{ $course->name }}</b>, {{ $course->teacher->fullname }}</td>
+                            @if($course->participants->first() && $course->participants->first()->state <> \App\Models\Participant::NOTHING)
+                                <td class="col-xs-6 text-center table_courses_view_td">
+                                    @if($course->participants->first()->state == \App\Models\Participant::ACCEPTED)
+                                        Prihlásený
+                                    @endif
+                                    @if($course->participants->first()->state == \App\Models\Participant::REJECTED)
+                                        Odmietnutý
+                                    @endif
+                                    @if($course->participants->first()->state == \App\Models\Participant::PENDING)
+                                        Čaká na schválenie
+                                    @endif
+                                </td>
+                            @else
+                                <td class="col-xs-6 text-center table_courses_view_button">
+                                    <button type="button" class="btn btn-default" data-course="{{ $course->id }}">
+                                        Prihlásiť sa
+                                    </button>
+                                </td>
+                            @endif
+                    </tr>
+                @endforeach
+            </tbody>
         </table>
         <div class="text-center">
             {!! $courses->render() !!}
--- resources/views/users/grading.blade.php
+++ resources/views/users/grading.blade.php
@@ -18,59 +18,67 @@
                 <div id="tab1" class="tab-pane fade in active">
                     @if(Auth::user()->hasRole(\App\Models\User::TEACHER_ROLE))
                         <table class="center_elements table-striped table_not_grading_teacher col-xs-12">
-                            @foreach($unratedArticles as $unratedArticle)
-                                <tr class="row">
-                                    <td class="border_right col-xs-6 table_not_grading_student_td">{{ $unratedArticle->title }}</td>
-                                    <td class="col-xs-6 table_not_grading_student_button text-center">
-                                        <a href="{{ action('ArticleController@getRate', ['id' => $unratedArticle->slug]) }}">
-                                            <button type="button" class="btn btn-default">
-                                                Ohodnotiť
-                                            </button>
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
+                            <tbody>
+                                @foreach($unratedArticles as $unratedArticle)
+                                    <tr class="row">
+                                        <td class="border_right col-xs-6 table_not_grading_student_td">{{ $unratedArticle->title }}</td>
+                                        <td class="col-xs-6 table_not_grading_student_button text-center">
+                                            <a href="{{ action('ArticleController@getRate', ['id' => $unratedArticle->slug]) }}">
+                                                <button type="button" class="btn btn-default">
+                                                    Ohodnotiť
+                                                </button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
                         </table>
                     @else
                         <table class="center_elements table-striped table_not_grading_student col-xs-12">
-                            @foreach($unratedArticles as $unratedArticle)
-                                <tr class="row">
-                                    <td class="col-xs-12">{!! link_to_action('ArticleController@getShow', $unratedArticle->title, ['id' => $unratedArticle->slug]) !!}</td>
-                                </tr>
-                            @endforeach
+                            <tbody>
+                                @foreach($unratedArticles as $unratedArticle)
+                                    <tr class="row">
+                                        <td class="col-xs-12">{!! link_to_action('ArticleController@getShow', $unratedArticle->title, ['id' => $unratedArticle->slug]) !!}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
                         </table>
                     @endif
                 </div>
                 <div id="tab2" class="tab-pane fade">
                     @if(Auth::user()->hasRole(\App\Models\User::TEACHER_ROLE))
                         <table class="center_elements table-striped table_grades col-xs-12">
-                            @foreach($ratedArticles as $ratedArticle)
-                                <tr class="row">
-                                    <td class="border_right col-xs-6">{{ \App\Models\User::find($ratedArticle->user_id)->fullname }}</td>
-                                    <td class="col-xs-6 text-center">
-                                        <input type="number" class="rating" min=0 max=5
-                                               step=1 readonly data-size="xs"
-                                               data-show-Caption="false" data-show-Clear="false"
-                                               value="{{ round($ratedArticle->rating) }}"
-                                               data-disabled="true">
-                                    </td>
-                                </tr>
-                            @endforeach
+                            <tbody>
+                                @foreach($ratedArticles as $ratedArticle)
+                                    <tr class="row">
+                                        <td class="border_right col-xs-6">{{ \App\Models\User::find($ratedArticle->user_id)->fullname }}</td>
+                                        <td class="col-xs-6 text-center">
+                                            <input type="number" class="rating" min=0 max=5
+                                                   step=1 readonly data-size="xs"
+                                                   data-show-Caption="false" data-show-Clear="false"
+                                                   value="{{ round($ratedArticle->rating) }}"
+                                                   data-disabled="true">
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
                         </table>
                     @else
                         <table class="center_elements table-striped table_grades col-xs-12">
-                            @foreach($ratedArticles as $ratedArticle)
-                                <tr class="row">
-                                    <td class="border_right col-xs-6">{!! link_to_action('ArticleController@getShow', $ratedArticle->title, ['id' => $ratedArticle->slug]) !!}</td>
-                                    <td class="col-xs-6  text-center">
-                                        <input type="number" class="rating" min=0 max=5
-                                               step=1 readonly data-size="xs"
-                                               data-show-Caption="false" data-show-Clear="false"
-                                               value="{{ round($ratedArticle->teacher_rating_value) }}"
-                                               data-disabled="true">
-                                    </td>
-                                </tr>
-                            @endforeach
+                            <tbody>
+                                @foreach($ratedArticles as $ratedArticle)
+                                    <tr class="row">
+                                        <td class="border_right col-xs-6">{!! link_to_action('ArticleController@getShow', $ratedArticle->title, ['id' => $ratedArticle->slug]) !!}</td>
+                                        <td class="col-xs-6  text-center">
+                                            <input type="number" class="rating" min=0 max=5
+                                                   step=1 readonly data-size="xs"
+                                                   data-show-Caption="false" data-show-Clear="false"
+                                                   value="{{ round($ratedArticle->teacher_rating_value) }}"
+                                                   data-disabled="true">
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
                         </table>
                     @endif
                 </div>
