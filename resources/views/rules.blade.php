5eb03d8d13b03c8769c48638bb4756126ce2771e
psiskova <pat.siskova@gmail.com>
2015-12-29 11:01:30 +0100

    update rules

--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -3,7 +3,16 @@
 @section('content')
     <div class="container-fluid row">
         <div class="col-md-8 col-md-offset-2 push container_content">
-            <h1>Pravidlá</h1>
+            <h1>Pravidlá blogovania na BlogAcademy.top</h1>
+            <ol>
+                <li>Blogerom na <a href="http://blogacademy.top/">BlogAcademy.top</a> môžu byť len žiaci a učitelia Gymnázia FMFI Mlynská dolina.</li>
+                <li>Každý bloger je povinný pri registrácii uviesť svoje pravé údaje: meno, priezvisko a e-mailovú adresu.</li>
+                <li>Bloger môže blogovať na zadanú tému v rámci kurzu, prípadne na voľnú tému, avšak svojimi príspevkami nesmie porušovať školský poriadok.</li>
+                <li>V textoch nesmú byť vulgarizmy, nepravdivé informácie, urážky.</li>
+                <li>Je zakázané publikovať články, ktoré propagujú násilie, agresivitu, vojny, rasovú, náboženskú, politickú, národnostnú či inú neznášanlivosť.</li>
+                <li>Bloger sa zaväzuje k tomu, že každý článok je jeho vlastné dielo.</li>
+                <li>Bloger nesmie porušovať autorské práva, nemôže citovať cudzie texty a používať cudzie fotografie bez toho, aby uviedol zdroj.</li>
+            </ol>
         </div>
     </div>
 @stop
