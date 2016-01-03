69b0edcd889368b4abfaf62462b7baf9774adc2b
MartinaJ <jamriskova6@uniba.sk>
2016-01-03 17:24:21 +0100

    edit aboutus

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -5,10 +5,18 @@
         <div class="col-md-8 col-md-offset-2 push container_content">
             <h1>O nás</h1>
             <div class="text-center">
-                <p class="font-large">Sme blogerská komunita pozostávajúca z učiteľov a žiakov Gymnázia FMFI Mlynská
-                    dolina</p>
-                <img src="https://web-school.in/wp-content/uploads/2015/03/SMS.gif" class="center-block"
-                     style="width: 50%" alt="School">
+                <p class="font-large">Sme blogerská komunita pozostávajúca z učiteľov a žiakov Gymnázia FMFI Mlynská dolina. </p>
+                <div class="row">
+                <img src="public/img/school.jpg" alt="Fotka budovy skoly" class="center-block col-md-7 hidden-xs" height="300">
+                    <div class="main_q col-md-5 center-block text-center">
+                        <blockquote class="main_quote font-large">
+                            <p>Keď nevieš - naučíme Ťa!</p>
+                            <p>Keď nevládzeš - pomôžeme Ti!</p>
+                            <p>Keď sa Ti nechce - zvládneme to bez Teba!</p>
+                        </blockquote>
+                    </div>
+                </div>
+
                 <div class="row bordered">
                     <div class="col-md-4">
                         máme {{ trans_choice('bloggers.count', $usersCount) }}
