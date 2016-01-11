a587d7bcbdf80ea1cb0e343fafdad3e61d829b52
stiffix <stiffsoftware@gmail.com>
2016-01-11 22:15:06 +0100

    Table fixed

--- resources/views/users/grading.blade.php
+++ resources/views/users/grading.blade.php
@@ -22,7 +22,7 @@
                     @if(Auth::user()->hasRole(\App\Models\User::TEACHER_ROLE))
                         <table class="center_elements table-striped table_not_grading_teacher col-xs-12">
                             <thead>
-                            <tr>
+                            <tr class="row">
                                 <th class="border_right col-xs-6">Názov článku</th>
                                 <th class="col-xs-6 text-center">Hodnotenie</th>
                             </tr>
@@ -45,7 +45,7 @@
                     @else
                         <table class="center_elements table-striped table_not_grading_student col-xs-12">
                             <thead>
-                                <tr>
+                                <tr class="row">
                                     <th class="col-xs-12">Názov článku</th>
                                 </tr>
                             </thead>
@@ -63,7 +63,7 @@
                     @if(Auth::user()->hasRole(\App\Models\User::TEACHER_ROLE))
                         <table class="center_elements table-striped table_grades col-xs-12">
                             <thead>
-                                <tr>
+                                <tr class="row">
                                     <th class="border_right col-xs-6">Meno a priezvisko</th>
                                     <th class="col-xs-6 text-center">Hodnotenie</th>
                                 </tr>
@@ -86,7 +86,7 @@
                     @else
                         <table class="center_elements table-striped table_grades col-xs-12">
                             <thead>
-                                <tr>
+                                <tr class="row">
                                     <th class="border_right col-xs-6">Názov článku</th>
                                     <th class="col-xs-6 text-center">Hodnotenie</th>
                                 </tr>
